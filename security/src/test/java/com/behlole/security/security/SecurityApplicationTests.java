bb6ddb94d043b10476d1a4db92427f5f73a4c3fa
behlole <behloleaqil@gmail.com>
2023-04-25 16:24:56 -0400

    spring security setup

---
+++ security/src/test/java/com/behlole/security/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behlole.security.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
